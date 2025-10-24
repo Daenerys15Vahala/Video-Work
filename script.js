591f5e6b02b99d94be1ec857615bc6183565a669
Valentina BM <valentina15bermo@gmail.com>
2025-10-24 12:53:07 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-console.log("Hello World");
+console.log("Hello world!");
 
-const myName = "Jonas Schmedtamnn";
+const myName = "Jonas Schmedtmann";
 const h1 = document.querySelector(".heading-primary");
 console.log(myName);
 console.log(h1);
@@ -11,20 +11,24 @@ console.log(h1);
 //   h1.style.padding = "5rem";
 // });
 
-//Set cuerrent year
+///////////////////////////////////////////////////////////
+// Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
-//////////////////////////////////////////////////////////////// Make mobile navigation work
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
 const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector("header");
+const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
-//////////////////////////////////////////////////////////Smooth scrolling animation
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
 
 const allLinks = document.querySelectorAll("a:link");
 
@@ -33,26 +37,27 @@ allLinks.forEach(function (link) {
     e.preventDefault();
     const href = link.getAttribute("href");
 
-    //Scroll back to top
-    if (href == "#")
+    // Scroll back to top
+    if (href === "#")
       window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
 
-    //Scroll to other links
+    // Scroll to other links
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
-    //Close mobile navigation
+    // Close mobile naviagtion
     if (link.classList.contains("main-nav-link"))
       headerEl.classList.toggle("nav-open");
   });
 });
 
-////////////////////////////////////////////////////////////////////////// STICKY NAVIGATION
+///////////////////////////////////////////////////////////
+// Sticky navigation
 
 const sectionHeroEl = document.querySelector(".section-hero");
 
@@ -70,7 +75,7 @@ const obs = new IntersectionObserver(
     }
   },
   {
-    //In the viewport
+    // In the viewport
     root: null,
     threshold: 0,
     rootMargin: "-80px",
